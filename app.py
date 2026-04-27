6ab2e11eb55c7fff52c0e61af41fb357d2a11cc6
bhagwatsatyam <bhagwatsatyam05@gmail.com>
2026-04-27 10:51:15 +0530

    test trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 registrations = []
-
+#test trigger
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
